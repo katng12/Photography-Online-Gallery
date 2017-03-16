5ae0e786f128686da5a6294b4b49746ed4d12161
katng12 <katnguyen2612@gmail.com>
2017-03-16 16:55:09 -0400

    modal updated

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
+//toggle menu
 function showToggle(x) {
     x.classList.toggle("change");
     x.classList.toggle("show-dropdown-content");
 };
 
-
+//make table for gallery section
 $(function() {
     $("#page-wrap").wrapInner("<table cellspacing='50'><tr>");
     $(".post").wrap("<td>");
- });
-
- $(document).ready(function () {
+    //scroll horizontally on mousewheel
     $('body').mousewheel(function(e, delta) {
         this.scrollLeft -= (delta * 40);
         e.preventDefault();
     });
+    //toggle about section
     $('.about-link').click(function() {
-    	$('.about').toggle();
+        $('.about').toggle();
     });
     $('.close-button').click(function() {
         $('.about').hide();
     });
-});
+    //show image title on hover
+    $('.overlay').hover(
+        function() {
+            $(this).children(".title").show();
+        }, function() {
+            $(this).children(".title").hide();
+        }
+    );
+    //show modal when image clicked
+    $('.overlay').on('click', function() {
+        var image = $(this).children("img").attr("src");
+        var caption = $(this).children("img").attr('alt');
+        $('#modal-img').attr("src", image);
+        $('#modal').show();
+    });
+    $('.close').on('click', function() {
+        $('#modal').hide();
+    });
+ });
+
+//MODAL************************
+// function openModal() {
+//     document.getElementById('modal').style.display = "block";
+// }
+
+// function closeModal() {
+//     document.getElementById('myModal').style.display = "none";
+// }
